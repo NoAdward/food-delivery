f7aca69197716edfeaf6a1dd420bdb0a158f3f7e
NoAdward <dima.tomchuk.99@gmail.com>
2023-06-01 22:24:36 +0300

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/UI/Header/Header'
 import Home from './views/Home/Home'
 import AboutMe from './views/AboutMe/AboutMe';
+import Project from './views/MyProjects/Project'
 
 
 
@@ -19,6 +20,7 @@ function App() {
         {/* <Route path='/aboutMe' element={<AboutMe />} /> */}
       </Routes>
       <AboutMe />
+      <Project />
     </>
   )
 }
--- src/views/MyProjects/Project.jsx
+++ src/views/MyProjects/Project.jsx
@@ -1,6 +1,74 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Project.scss";
+
+import { EffectCoverflow, Pagination, Navigation } from "swiper";
+
+import slider_1 from "/src/assets/slider_1.png";
+import slider_2 from "/src/assets/slider_2.png";
+import slider_3 from "/src/assets/slider_3.png";
+
 const Project = () => {
-    return (
-      <></>
-    );
-  };
-  export default Project;
+  return (
+    <div className="container">
+      <h1 className="heading">My projects</h1>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slider_1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider_2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider_3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider_1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider_2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider_3} alt="" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Project;
